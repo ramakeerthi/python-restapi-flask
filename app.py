4f2dbe0854e18a28370cded002759003b0f7da88
Ramakeerthi <ramakeerthi09@gmail.com>
2024-03-26 12:45:40 +0530

    Updated DB structure to improve the way we access data

--- app.py
+++ app.py
@@ -1,58 +1,47 @@
 from flask import Flask,request
+from db import stores,items
+import uuid
 
 app = Flask(__name__)
-
-stores = [
-    {
-        "name": "My Store",
-        "items":[
-            {
-                "name":"Chair",
-                "price":1500
-            }
-        ]
-    }
-]
-
 
 # http://127.0.0.1:5000/store
 @app.get("/store")
 def get_stores():
-    return{"stores":stores}
+    return{"stores":list(stores.values())}
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name":request_data['name'],"items":[]}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data,"id":store_id}
+    stores[store_id]=new_store
     return new_store, 201
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            item = {
-                "name":request_data["name"],
-                "price":request_data["price"]
-            }
-            store["items"].append(item)
-            return item, 201
-    return {"message":"Store not found"},404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message":"store not found"},404
+    item_id = uuid.uuid4().hex
+    new_item = {**item_data,"id":item_id}
+    items[item_id]=new_item
+    
+    return new_item, 201
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message":"Store not found"},404
+@app.get("/item")
+def get_all_items():
+    return {"items":list(items.values())}
 
-@app.get("/store/<string:name>/item/<string:item_name>")
-def get_item(name,item_name):
-    for store in stores:
-        if store["name"] == name:
-            items = store["items"]
-            for item in items:
-                if item["name"]==item_name:
-                    return item
-    return {"message":"Store not found"},404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message":"Store not found"}, 404
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message":"item not found"},404
